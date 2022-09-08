12b992cf32083ecb2e7289beec362e0fffd185ef
elias <eliasfaculdade@yahoo.com.br>
2022-09-08 14:18:59 -0300

    up app

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using herois.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+//add
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+//add
+async Task<List<SuperHero>> GetAllHeroes(DataContext context) =>
+       await context.Superheros.ToListAsync();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+//add
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/superhero", async (DataContext context) => 
+    await context.Superheros.ToListAsync());
+
+//hero id
+app.MapGet("/superhero/{id}", async (DataContext context, int id) =>
+ await context.Superheros.FindAsync(id) is SuperHero hero ?
+ Results.Ok (hero) :
+ Results.NotFound("Sorry :("));
+
+//Post
+app.MapPost("/superhero", async (DataContext context, SuperHero hero) =>
+{
+    context.Superheros.Add(hero);
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllHeroes(context));//Ok(hero);
+});
+
+//Put
+app.MapPut("/superhero/{id}", async (DataContext context, SuperHero hero, int id) =>
+{
+    var dbHero = await context.Superheros.FindAsync(id);
+    if (dbHero == null)
+        return Results.NotFound("No hero found. :(");
+
+    dbHero.Firstname = hero.Firstname;
+    dbHero.Lastname = hero.Lastname;
+    dbHero.Heroname = hero.Heroname;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await GetAllHeroes(context));
+});
+
+//DELETE
+app.MapDelete("/superhero/{id}", async (DataContext context, int id) =>
+{
+    var dbHero = await context.Superheros.FindAsync(id);
+    if (dbHero == null)
+        return Results.NotFound(" :(");
+    context.Superheros.Remove(dbHero);
+    return Results.Ok(await GetAllHeroes(context));
+}); 
+
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+para rodar essa API:
+1 rodar migration para criar o banco de dados (dotnet ef database update)
